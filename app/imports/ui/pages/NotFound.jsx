ce19e6b6a9ffbdd3c84b1efd0e08218021a45b9b
YAMADA Masaki <masaki@metabolics.co.jp>
2021-02-20 14:27:54 +0900

    残りのページも i18n した
    これ以外のページは実際には使われないので, このままにしておく

--- app/imports/ui/pages/NotFound.jsx
+++ app/imports/ui/pages/NotFound.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
+import { withTranslation } from 'react-i18next'
+import PropTypes from 'prop-types'
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 class NotFound extends React.Component {
   render() {
+    const { t } = this.props
+
     return (
         <Header as="h2" textAlign="center">
-          <p>Page not found</p>
+          <p>{t('Page not found')}</p>
         </Header>
     )
   }
 }
 
-export default NotFound
+NotFound.propTypes = {
+  t: PropTypes.func,
+}
+
+export default withTranslation()(NotFound)
--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { Header } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+import { withTranslation } from 'react-i18next'
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
-export default class Signout extends React.Component {
+class Signout extends React.Component {
   render() {
+    const { t } = this.props
+
     Meteor.logout()
     return (
         <Header as="h2" textAlign="center">
-          <p>You are signed out.</p>
+          <p>{t('You are signed out')}</p>
         </Header>
     )
   }
 }
+
+Signout.propTypes = {
+  t: PropTypes.func,
+}
+
+export default withTranslation()(Signout)
