19c3a67a34c2370c5346836c4840c7bd8e60cc9e
YAMADA Masaki <masaki@metabolics.co.jp>
2021-04-04 17:36:56 +0900

    NavBar を reformat した

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Meteor } from 'meteor/meteor'
-import { withTracker } from 'meteor/react-meteor-data'
-import { NavLink, withRouter } from 'react-router-dom'
-import { Dropdown, Header, Menu } from 'semantic-ui-react'
-import { Roles } from 'meteor/alanning:roles'
-import { withTranslation } from 'react-i18next'
+import {Meteor} from 'meteor/meteor'
+import {withTracker} from 'meteor/react-meteor-data'
+import {NavLink, withRouter} from 'react-router-dom'
+import {Dropdown, Header, Menu} from 'semantic-ui-react'
+import {Roles} from 'meteor/alanning:roles'
+import {withTranslation} from 'react-i18next'
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
-    const { t, currentUser } = this.props
-    const { applicationName, selfRegistration } = Meteor.settings.public
-    const menuStyle = { marginBottom: '10px' }
+    const {t, currentUser} = this.props
+    const {applicationName, selfRegistration} = Meteor.settings.public
+    const menuStyle = {marginBottom: '10px'}
     return (
         <Menu style={menuStyle} attached="top" borderless inverted>
           <Menu.Item as={NavLink} activeClassName="" exact to="/">
             <Header inverted as='h1'>{applicationName}</Header>
           </Menu.Item>
-         {currentUser ? (
-             <Menu.Item>
-               <Dropdown text='Stuff'>
-                 <Dropdown.Menu>
-                   <Dropdown.Item text='add' as={NavLink} exact to="/add"/>
-                   <Dropdown.Item text='list' as={NavLink} exact to="/list"/>
-                 </Dropdown.Menu>
-               </Dropdown>
-             </Menu.Item>
-         ) : ''}
+          {currentUser ? (
+              <Menu.Item>
+                <Dropdown text='Stuff'>
+                  <Dropdown.Menu>
+                    <Dropdown.Item text='add' as={NavLink} exact to="/add"/>
+                    <Dropdown.Item text='list' as={NavLink} exact to="/list"/>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Menu.Item>
+          ) : ''}
           {Roles.userIsInRole(Meteor.userId(), 'admin') ? ([
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>,
             <Menu.Item as={NavLink} activeClassName="active" exact to="/users" key='users'>{t('Users')}</Menu.Item>,
