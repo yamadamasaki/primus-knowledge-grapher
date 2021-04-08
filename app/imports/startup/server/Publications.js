4c2cd18eaf4012e2418260095ecd475a922faf44
YAMADA Masaki <masaki@metabolics.co.jp>
2021-04-08 22:38:21 +0900

    Publications.js を reformat code した

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,13 +1,13 @@
-import { Meteor } from 'meteor/meteor'
-import { Stuffs } from '../../api/stuff/StuffCollection'
+import {Meteor} from 'meteor/meteor'
+import {Stuffs} from '../../api/stuff/StuffCollection'
 
 /** Publish all the collections you need. */
 Stuffs.publish()
 
 /** Need this for the alanning:roles package */
-Meteor.publish(null, function () {
+Meteor.publish(null, function() {
   if (this.userId) {
-    return Meteor.roleAssignment.find({ 'user._id': this.userId })
+    return Meteor.roleAssignment.find({'user._id': this.userId})
   }
   return this.ready()
 })
