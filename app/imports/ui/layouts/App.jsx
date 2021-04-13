668c14eaebe7370f6fb0fc2ff0cefb6e4c9fa8f4
YAMADA Masaki <masaki@metabolics.co.jp>
2021-04-13 21:26:40 +0900

    Program の一覧を表示するページを作った
    まだ create もできないので, 何も表示されない (一部のみ用意はされている = ProgramModal)

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -16,7 +16,7 @@ import Signin from '../pages/Signin'
 import Signup from '../pages/Signup'
 import Signout from '../pages/Signout'
 import Users from '../pages/Users'
-import Programs from '../pages/Programs'
+import ProgramPage from '../pages/ProgramPage'
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -31,7 +31,7 @@ class App extends React.Component {
               <Route path="/signup" component={Meteor.settings.public.selfRegistration ? Signup : NotFound}/>
               <ProtectedRoute path="/list" component={ListStuff}/>
               <ProtectedRoute path="/add" component={AddStuff}/>
-              <ProtectedRoute path="/programs" component={Programs}/>
+              <ProtectedRoute path="/programs" component={ProgramPage}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
               <AdminProtectedRoute path="/users" component={Users}/>
--- app/imports/ui/pages/ProgramPage.jsx
+++ app/imports/ui/pages/ProgramPage.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import {useTracker} from 'meteor/react-meteor-data'
+import {Programs} from '../../api/program/ProgramCollection'
+import {Container, Header, Loader} from 'semantic-ui-react'
+import {useTranslation} from 'react-i18next'
+import ProgramModal from '../components/ProgramModal'
 
-const Programs = () => {
-  const programs = useTracker()
-  return (<h1>Programs</h1>)
+const ProgramPage = () => {
+  const {t, i18n} = useTranslation()
+  const programsLoading = useTracker(() => !Programs.subscribe(Programs.getChannels().allWithMeta).ready())
+  const programs = useTracker(() => Programs.find().fetch())
+
+  return (
+      <Container>
+        <Header as="h2">{t('Programs')}</Header>
+        <ProgramModal/>
+        {
+          programsLoading ?
+              <Loader/> :
+              <ul>
+                {
+                  programs.map((item, index) => <li key={index}>{item.title}</li>)
+                }
+              </ul>
+        }
+      </Container>
+  )
 }
 
-export default Programs
+export default ProgramPage
