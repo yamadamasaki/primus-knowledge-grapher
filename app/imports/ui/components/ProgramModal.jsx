f934098a60a962a1f908624f2497aafd68f6ac35
YAMADA Masaki <masaki@metabolics.co.jp>
2021-04-17 13:51:25 +0900

    ProgramModal の呼び出しを自前の trigger ではなく, 呼び出し側の [modalOpen, setModalOpen] を使って行うようにした
    ProgramPage (create) と ProgramTable (update) の両方から呼び出せるようにするため.
    trigger を呼び出し側に埋め込むと, width が > 1000 になってしまうので.

--- app/imports/ui/components/ProgramModal.jsx
+++ app/imports/ui/components/ProgramModal.jsx
@@ -1,26 +1,24 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {useTranslation} from 'react-i18next'
-import {Button, Container, Header, Modal} from 'semantic-ui-react'
+import {Button, Container, Modal} from 'semantic-ui-react'
 import ProgramForm from './ProgramForm'
 
-const ProgramModal = () => {
+const ProgramModal = ({modalOpen, setModalOpen, model}) => {
   const {t} = useTranslation()
-  const [open, setOpen] = useState()
 
   return (
       <Container>
         <Modal
-            onOpen={() => setOpen(true)}
-            onClose={() => setOpen(false)}
-            open={open}
-            trigger={<Button primary>{t('Add')}</Button>}
+            onOpen={() => setModalOpen(true)}
+            onClose={() => setModalOpen(false)}
+            open={modalOpen}
         >
           <Modal.Header>{t('Create a Program')}</Modal.Header>
           <Modal.Content>
-            <ProgramForm closer={setOpen}/>
+            <ProgramForm closer={setModalOpen} model={model}/>
           </Modal.Content>
           <Modal.Actions>
-            <Button color="black" onClick={() => setOpen(false)}>{t('Cancel')}</Button>
+            <Button color="black" onClick={() => setModalOpen(false)}>{t('Cancel')}</Button>
           </Modal.Actions>
         </Modal>
       </Container>
--- app/imports/ui/pages/ProgramPage.jsx
+++ app/imports/ui/pages/ProgramPage.jsx
@@ -1,17 +1,19 @@
-import React, {useMemo} from 'react'
+import React, {useMemo, useState} from 'react'
 import {Programs} from '../../api/program/ProgramCollection'
-import {Container, Header, Loader} from 'semantic-ui-react'
+import {Button, Container, Header, Loader} from 'semantic-ui-react'
 import {useTranslation} from 'react-i18next'
 import ProgramModal from '../components/ProgramModal'
 import ProgramTable from '../components/ProgramTable'
 
 const ProgramPage = () => {
   const {t} = useTranslation()
+  const [modalOpen, setModalOpen] = useState(false)
 
   return (
       <Container>
         <Header as="h2">{t('Programs')}</Header>
-        <ProgramModal/>
+        <ProgramModal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
+        <Button primary onClick={() => setModalOpen(true)}>{t('Add')}</Button>
         <ProgramTable/>
       </Container>
   )
