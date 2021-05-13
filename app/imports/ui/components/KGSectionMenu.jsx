10c9b54d7b692f2b20c129eb9a37d72e26ed491e
YAMADA Masaki <masaki@metabolics.co.jp>
2021-05-13 18:49:04 +0900

    KGSectionMenu を作って, CFPrepSession に入れた
    チャットについては, 後回しにする

---
+++ app/imports/ui/components/KGSectionMenu.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const boxStyle = {
+  margin: '10px 10px 10px 10px',
+  border: '1px solid #afadad',
+  borderRadius: '5px',
+  padding: '5px',
+}
+
+const KGSectionMenu = ({sectionNames, sections}) => (
+    <div>
+      {
+        sections ? (
+            <div style={boxStyle}>
+              <ol>
+                {
+                  sections.map((section, index) => {
+                    const {name, programId, sessionId, subsession, componentName} = section
+                    return (
+                        <li key={index}>
+                          <Link to={`/sessions/${programId}/${componentName}/${sessionId}/${subsession}`}>
+                            {name}
+                          </Link>
+                        </li>
+                    )
+                  })
+                }
+              </ol>
+            </div>
+        ) : <div/>
+      }
+      {
+        sectionNames ? (
+            <div style={boxStyle}>
+              <ol>
+                {
+                  sectionNames.map(
+                      (sectionName, index) => (<li key={index}><a href={`#${sectionName}`}>{sectionName}</a></li>)
+                  )
+                }
+              </ol>
+            </div>
+        ) : <div/>
+      }
+    </div>
+)
+
+export default KGSectionMenu
--- app/imports/ui/copernicusForum/CFPrepSession.jsx
+++ app/imports/ui/copernicusForum/CFPrepSession.jsx
@@ -9,6 +9,7 @@ import KGBreadCrumbs from '../components/KGBreadCrumbs'
 import ProgramIndexMenu from '../components/ProgramIndexMenu'
 import KGSessionHeader from '../components/KGSessionHeader'
 import KGSessionStart from '../components/KGSessionStart'
+import KGSectionMenu from '../components/KGSectionMenu'
 
 const sessionName = '準備セッション'
 const sessionComponentName = 'CFPrepSession'
@@ -82,10 +83,10 @@ const CFPrepSession = () => {
 
   const dismissError = () => history.goBack()
   const ShowError = () => (
-      error ?
+      error && (
           <Message onDismiss={dismissError} header={t('Error')}
-                   content={t('Unregistered Component', {componentName})}/> :
-          <div/>
+                   content={t('Unregistered Component', {componentName})}/>
+      )
   )
 
   return (
@@ -102,9 +103,18 @@ const CFPrepSession = () => {
                             program={program} sessionName={sessionName} sessionComponent={sessionComponentName}/>
                         <KGSessionHeader sessionName={sessionName}/>
                         <KGSessionStart {...params} specs={specs} isStartable={{groups: ['admin']}}>
-                          KGSessionStart
-                        {/*    KGSectionMenu*/}
-                        {/*    KGChatButton*/}
+                          <KGSectionMenu sections={sections}/>
+                          {/*
+                              <Components.KGChatButton match={{
+                                params: {
+                                  programId,
+                                  sessionId,
+                                  subsession: 'prep-chat',
+                                  isChattable: {groups: ['member']},
+                                  isReadable: {groups: ['member']},
+                                },
+                              }}/>
+                            */}
                         </KGSessionStart>
                       </div>
                     </ProgramIndexMenu>
@@ -116,79 +126,3 @@ const CFPrepSession = () => {
 }
 
 export default CFPrepSession
-
-/*
-const CFPrepSession = ({match}) => {
-  const {params} = match
-  const {programId, sessionId} = params
-
-  const sections = [
-    {name: 'ねらい', programId, sessionId, subsession: 'prep-guidance', componentName: 'KGTextDiagramSubsession'},
-    {name: '課題', programId, sessionId, subsession: 'prep-questionnaire', componentName: 'KGAssignmentSubsession'},
-    {name: 'アサイン', programId, sessionId, subsession: 'prep-questionnaire', componentName: 'KGTeamsSubsession'},
-  ]
-
-  const spec = {
-    sessionName,
-    sessionComponentName,
-    sections,
-    'prep-guidance': {
-      sectionName: 'ねらい',
-      diagramComponentName: 'CFFrameworkDiagramSection',
-      isTextEditable: {groups: ['admin']},
-      isTextReadable: {groups: ['member']},
-      isDiagramSavable: {groups: ['admin']},
-      isDiagramReadable: {groups: ['member']},
-    },
-    'prep-questionnaire': {
-      sectionName: '課題',
-      diagramComponentName: 'CFFrameworkDiagramSection',
-      isTeamDefinable: {groups: ['admin']},
-      isTeamAnswerable: {groups: ['member']}, // Not Implemented Yet
-      delegatedComponentName: 'KGAnswerSubsession',      // 以下は問い掛け部分の permission
-      // isText... があれば showText: true, isDiagram... があれば showDiagram: true
-      isTextEditable: {groups: ['admin']},
-      isTextReadable: {groups: ['member']},
-      isDiagramSavable: {groups: ['admin']},
-      isDiagramReadable: {groups: ['member']},
-      // 課題成果物の permission はその先で teamId で決める
-    },
-    'prep-questionnaire-answer': {
-      sectionName: '課題',
-      diagramComponentName: 'CFFrameworkDiagramSection',
-      isTextEditable: {groups: ['admin']},
-      isTextReadable: {groups: ['member']},
-      isDiagramSavable: {groups: ['admin']},
-      isDiagramReadable: {groups: ['member']},
-    },
-  }
-
-  return (
-      <React.Fragment>
-        <Helmet><title>Prep Session ({sessionId})</title></Helmet>
-        <Components.KGBreadCrumbs programId={programId} sessionId={sessionId}/>
-        <Components.KGSessionHeader sessionName={sessionName}/>
-
-        <Components.KGSessionStart programId={programId} sessionId={sessionId} spec={spec}
-                                   isStartable={{groups: ['admin']}}>
-          <React.Fragment>
-            <Components.KGSectionMenu sections={sections}/>
-
-            <Components.KGChatButton match={{
-              params: {
-                programId,
-                sessionId,
-                subsession: 'prep-chat',
-                isChattable: {groups: ['member']},
-                isReadable: {groups: ['member']},
-              },
-            }}/>
-          </React.Fragment>
-        </Components.KGSessionStart>
-
-      </React.Fragment>
-  )
-}
-
-registerComponent({name: 'CFPrepSession', component: CFPrepSession})
-*/
