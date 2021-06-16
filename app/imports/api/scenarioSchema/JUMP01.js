15c08db966feea7ef68226d0a2a09623aae97ffd
YAMADA Masaki <masaki@metabolics.co.jp>
2021-06-16 17:51:24 +0900

    JUMP の例題プログラムを追加した

---
+++ app/imports/api/scenarioSchema/JUMP01.js
@@ -0,0 +1,60 @@
+import SimpleSchema from 'simpl-schema'
+
+const session = {
+  name: String,
+  id: String,
+  componentName: {
+    type: String,
+    allowedValues: [
+      'JumpVisionSession',
+      'JumpBHAGSession',
+      'JumpMVESession',
+      'JumpQuarterlySession',
+      'JumpMonthlySession',
+      'JumpWeeklySession',
+    ],
+    optional: true,
+  },
+}
+
+// javascript, simple-schema の制約により, セッションのネストは最大 5 レベルとする
+const sessionLevel5 = new SimpleSchema({
+  ...session,
+})
+
+const sessionLevel4 = new SimpleSchema({
+  ...session,
+  children: {type: Array, optional: true},
+  'children.$': sessionLevel5,
+})
+
+const sessionLevel3 = new SimpleSchema({
+  ...session,
+  children: {type: Array, optional: true},
+  'children.$': sessionLevel4,
+})
+
+const sessionLevel2 = new SimpleSchema({
+  ...session,
+  children: {type: Array, optional: true},
+  'children.$': sessionLevel3,
+})
+
+const sessionLevel1 = new SimpleSchema({
+  ...session,
+  children: {type: Array, optional: true},
+  'children.$': sessionLevel2,
+})
+
+const schema = new SimpleSchema({
+  indexComponent: {type: String, allowedValues: ['CFHomePage']},
+  title: String,
+  subtitle: {type: String, optional: true},
+  fromDate: {type: String, optional: true},
+  toDate: {type: String, optional: true},
+  organization: {type: String, optional: true},
+  children: {type: Array, optional: true},
+  'children.$': sessionLevel1,
+})
+
+export default schema
--- app/imports/api/scenarioSchema/index.js
+++ app/imports/api/scenarioSchema/index.js
@@ -1,7 +1,9 @@
 import CopernicusForum01 from './CopernicusForum01'
 import Test01 from './Test01'
+import JUMP01 from './JUMP01'
 
 export const ScenarioSchemata = {
   CopernicusForum01,
   Test01,
+  JUMP01,
 }
